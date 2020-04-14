d6611835ff803cedc345ddccfbcd9d900f388b62
Estefan Hu <estefanhu074@gmail.com>
2020-04-14 03:11:53 -0700

    added edit route

--- routes/articles.js
+++ routes/articles.js
@@ -5,6 +5,11 @@ router.route('/new').get((req, res) => {
   res.render('./articles/new', { article: new Article() });
 });
 
+router.route('/edit/:id').get(async (req, res) => {
+  const article = await Article.findById(req.params.id);
+  res.render('./articles/edit', { article: article });
+});
+
 router.route('/:slug').get(async (req, res) => {
   const article = await Article.findOne({ slug: req.params.slug });
   if (article == null) res.redirect('/');
