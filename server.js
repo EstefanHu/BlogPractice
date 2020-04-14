1eea96f2c6e84806f903b8b25f8596559c8c2d4a
Estefan Hu <estefanhu074@gmail.com>
2020-04-14 01:39:50 -0700

    started testing EJS rendering

--- server.js
+++ server.js
@@ -3,4 +3,16 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+const articleRouter = require('./routes/articles');
+app.use('/articles', articleRouter);
+
+app.get('/', (req, res) => {
+  const articles = [{
+    title: 'Test article',
+    createdAt: Date.now(),
+    description: 'Test Description'
+  }];
+  res.render('index', { articles: articles });
+});
+
 app.listen('4000', () => console.log('Listening on Port 4000'));
