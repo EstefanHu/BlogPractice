684fea735d46c701742ce38715fd47ce5334fe46
Estefan Hu <estefanhu074@gmail.com>
2020-04-14 02:56:47 -0700

    Added slugify

--- routes/articles.js
+++ routes/articles.js
@@ -5,8 +5,8 @@ router.route('/new').get((req, res) => {
   res.render('./articles/new', { article: new Article() });
 });
 
-router.route('/:id').get(async (req, res) => {
-  const article = await Article.findById(req.params.id);
+router.route('/:slug').get(async (req, res) => {
+  const article = await Article.findOne({ slug: req.params.slug });
   if (article == null) res.redirect('/');
   res.render('articles/show', { article: article });
 });
@@ -20,7 +20,7 @@ router.route('/').post(async (req, res) => {
 
   try {
     article = await article.save();
-    res.redirect(`/articles/${article.id}`);
+    res.redirect(`/articles/${article.slug}`);
   } catch(error) {
     console.log(error);
     res.render('articles/new', { article: article });
