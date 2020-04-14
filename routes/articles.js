81ed971d4d4c088e2af6c623c9cafc479711dc22
Estefan Hu <estefanhu074@gmail.com>
2020-04-14 02:31:36 -0700

    installed article getting route

--- routes/articles.js
+++ routes/articles.js
@@ -5,12 +5,14 @@ router.route('/new').get((req, res) => {
   res.render('./articles/new', { article: new Article() });
 });
 
-router.route('/:id').get((req, res) => {
-
+router.route('/:id').get(async (req, res) => {
+  const article = await Article.findById(req.params.id);
+  if (article == null) res.redirect('/');
+  res.render('articles/show', { article: article });
 });
 
 router.route('/').post(async (req, res) => {
-  const article = new Article({
+  let article = new Article({
     title: req.body.title,
     description: req.body.description,
     markdown: req.body.markdown
@@ -20,6 +22,7 @@ router.route('/').post(async (req, res) => {
     article = await article.save();
     res.redirect(`/articles/${article.id}`);
   } catch(error) {
+    console.log(error);
     res.render('articles/new', { article: article });
   }
 });
